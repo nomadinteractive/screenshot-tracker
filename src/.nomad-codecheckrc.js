5b09456c85215bcfd8f23c150531b373139c87e2
Mehmet Fatih Yıldız <fatih@mfyz.com>
2020-02-11 00:34:22 -0500

    feat: pre-commit hooks

---
+++ src/.nomad-codecheckrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "rules": [
+        // "branch-name-lint",
+        // "no-assets-outside-assets-folder",
+        // "no-network-request-outside-network-managers-folder",
+        // "no-storage-outside-persistent-data-managers-folder",
+    ]
+}
